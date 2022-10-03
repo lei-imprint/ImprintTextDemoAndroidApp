a859a7d19deec2ccfff6d25ce57ca21152040b75
Lei Cui <lei@imprint.co>
2022-10-03 09:26:29 -0700

    init commit

---
+++ app/src/main/java/com/example/richtextapplication/ImprintText.kt
@@ -0,0 +1,123 @@
+package com.example.richtextapplication
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.gson.annotations.SerializedName
+
+
+/**
+ * All-in-one infra that can hold versatile texts in a single block, with custom styles at different
+ * places; such as:
+ * one block of text with title + n paragraphs of descriptions;
+ * one snippet text with n clickable links;
+ * or any of the above combined;
+ *
+ * The goal is to make data totally controlled by server side, and client side can parse and render
+ * the data without ZERO code change;
+ *
+ * This is not exclusive set of configurations of text, instead, it is more of a working demo to
+ * show the solution how to remotely define and control complex string and strings. Once added into
+ * codebase, the code quality and Unit Test should be addressed; Also, DarkTheme need to be handled
+ * properly;
+ *
+ * For completed configuration of text level (not layout level), can refer to params in [SpanStyle]
+ *
+ * Usage steps:
+ * 1. sever side define and pass down imprint_text_data;
+ * 2. client side parse imprint_text_data into [ImprintTextData];
+ * 3. client side bind [ImprintTextData] with [ImprintText];
+ * */
+
+@Composable
+fun ImprintText(data: ImprintTextData) {
+
+  fun getMap() = data.args.filter { it.link != null }.associate { it.string to it.link }
+
+  fun buildStyledSpan(data: ImprintTextData) =
+    SpanStyle(
+      fontSize = data.fontSize.sp,
+      color = Color(data.color),
+      fontWeight = if (data.bold) FontWeight.Bold else null,
+      textDecoration = if (data.underline) TextDecoration.Underline else null,
+    )
+
+  fun buildText() = buildAnnotatedString {
+    val str = data.string
+    val countArgs = str.count { c -> c == '%' }
+    if (countArgs != data.args.size) {
+      print("Args Not match!")
+      return AnnotatedString("")
+    }
+    var last = 0
+    data.args.forEachIndexed { index, child ->
+      val placeholder = "{%s$index}"
+      val p1 = str.indexOf(placeholder)
+      withStyle(buildStyledSpan(data)) { append(str.substring(last, p1)) }
+      if (child.link != null) {
+        pushStringAnnotation(
+          tag = child.string,
+          annotation = child.link,
+        )
+        withStyle(buildStyledSpan(child)) { append(child.string) }
+        pop()
+      } else {
+        withStyle(buildStyledSpan(child)) { append(child.string) }
+      }
+      last = p1 + placeholder.length
+    }
+    withStyle(buildStyledSpan(data)) { append(str.substring(last)) }
+  }
+
+  val annotatedText = buildText()
+  val uriHandler = LocalUriHandler.current
+  var modifier = Modifier
+    .background(color = Color(data.background))
+    .padding(data.padding.dp)
+  if (data.layoutWidth == -1L) {
+    modifier = modifier.fillMaxWidth()
+  } else if (data.layoutWidth > 0) {
+    modifier = modifier.width(data.layoutWidth.toInt().dp)
+  }
+
+  ClickableText(
+    text = annotatedText,
+    modifier = modifier,
+    onClick = { offset ->
+      for (tag in getMap().keys) {
+        annotatedText
+          .getStringAnnotations(tag = tag, start = offset, end = offset)
+          .firstOrNull()?.let { uriHandler.openUri(getMap()[tag]!!) }
+      }
+    }
+  )
+}
+
+data class ImprintTextData(
+  @SerializedName("string") val string: String = "",
+  @SerializedName("font_size") val fontSize: Int = 14,
+  @SerializedName("color") val color: Long = 0xFF000000,
+  @SerializedName("bold") val bold: Boolean = false,
+  @SerializedName("underline") val underline: Boolean = false,
+  @SerializedName("link") val link: String? = null,
+  @SerializedName("background") val background: Long = 0x00000000, // only applied at top level
+  // 0 = wrap content; 1 = match parent; > 0 means custom width; only applied at top level
+  @SerializedName("layout_width") val layoutWidth: Long = 0,
+  @SerializedName("padding") val padding: Int = 0, // only applied at top level
+  // can have 2nd level embedded texts with different styles, can be used such as: title, link
+  @SerializedName("args") val args: List<ImprintTextData> = emptyList(),
+)
--- app/src/main/java/com/example/richtextapplication/MainActivity.kt
+++ app/src/main/java/com/example/richtextapplication/MainActivity.kt
@@ -3,21 +3,15 @@ package com.example.richtextapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.richtextapplication.ui.theme.RichTextApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,65 +20,67 @@ class MainActivity : ComponentActivity() {
     setContent {
       RichTextApplicationTheme {
         // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          RichText(
-            data = RichTextData(
-              string = "hello {%s0}, this is a {%s1}",
-              color = 0xFFFFFF00,
-              args = listOf(
-                RichTextData("world", 0xFFCCCCCC),
-                RichTextData(
-                  "link",
-                  fontSize = 26,
-                  bold = true,
-                  color = 0xFFFF0000,
-                  link = "https://www.google.com/"
-                )
-              )
-            )
-          )
+        Surface(
+          modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+            .padding(12.dp),
+          color = MaterialTheme.colors.background
+        ) {
+          Column {
+            ImprintText(data = test1)
+            Divider(Modifier.padding(0.dp, 32.dp))
+            ImprintText(data = test2)
+            Divider(Modifier.padding(0.dp, 32.dp))
+            ImprintText(data = test3)
+          }
         }
       }
     }
   }
 }
 
-data class RichTextData(
-  val string: String,
-  val color: Long = 0xFF000000,
-  val bold: Boolean = false,
-  val fontSize: Int = 14,
-  val link: String? = null,
-  val args: List<RichTextData> = emptyList(),
+val test1 = ImprintTextData(
+  string = "Hello {%s0}\nThis is a {%s1}",
+  background = 0x220000FF,
+  layoutWidth = 200L,
+  padding = 8,
+  args = listOf(
+    ImprintTextData(
+      string = "Imprint",
+      color = 0xFF0000FF,
+      fontSize = 20,
+      link = "https://www.imprint.co/"
+    ),
+    ImprintTextData(
+      string = "Apple",
+      fontSize = 26,
+      bold = true,
+      color = 0xFFFF0000,
+      link = "https://www.apple.com/"
+    )
+  )
 )
 
-@Composable
-fun RichText(data: RichTextData) {
+val test2 = ImprintTextData(
+  string = "Hello, this is a plain text, with only string, and there is no any other configuration",
+)
 
-  fun buildStyledSpan(data: RichTextData) =
-    SpanStyle(
-      fontSize = data.fontSize.sp,
-      color = Color(data.color),
-      fontWeight = if (data.bold) FontWeight.Bold else FontWeight.Normal
+val test3 = ImprintTextData(
+  string = "{%s0}\n\n{%s1}{%s2}.",
+  args = listOf(
+    ImprintTextData("Title", fontSize = 20, bold = true),
+    ImprintTextData(
+      string = "This is description, This is description, This is description, ",
+      fontSize = 16,
+      color = 0xFF888888,
+    ),
+    ImprintTextData(
+      "more info",
+      fontSize = 16,
+      color = 0xFF888888,
+      underline = true,
+      link = "https://www.imprint.co/",
     )
-
-  fun buildString() = buildAnnotatedString {
-    val str = data.string
-    val countArgs = str.count { c -> c == '%' }
-    if (countArgs != data.args.size) {
-      print("Args Not match!")
-      return AnnotatedString("")
-    }
-    var last = 0
-    data.args.forEachIndexed { index, child ->
-      val placeholder = "{%s$index}"
-      var p1 = str.indexOf(placeholder)
-      withStyle(buildStyledSpan(data)) { append(str.substring(last, p1)) }
-      withStyle(buildStyledSpan(child)) { append(child.string) }
-      last = p1 + placeholder.length
-    }
-    withStyle(buildStyledSpan(data)) { append(str.substring(last)) }
-  }
-
-  Text(buildString())
-}
+  )
+)
